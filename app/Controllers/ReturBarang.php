e2ce5311f248a07814b612bd06405358ec18cfe0
haisyamm <70573490+haisyamm@users.noreply.github.com>
2021-05-06 12:00:11 +0700

    Update ReturBarang.php

--- app/Controllers/ReturBarang.php
+++ app/Controllers/ReturBarang.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\Retur_model;
+
 class ReturBarang extends BaseController
 {
 
@@ -24,17 +26,45 @@ class ReturBarang extends BaseController
 	public function save()
     {
       
-        // if(!empty($cek)){
+        
 
-     
+		$model = new Retur_model();
+		//$pmodel = new Product_model();
 
-            echo '200';
+		$baris = $this->request->getPost('baris');
+		$no = '23';
+		$dataHeader = array(
+			'id_retur'        => $no,
+			'tgl_retur'       => $this->request->getPost('tglretur'),
+			'jenis_retur'        => $this->request->getPost('namacustomer'),
+			'alasan_retur' => $this->request->getPost('catatan'),
+			'total_retur' => $this->request->getPost('Total')
+			
+		);
+		$data = $model->saveRetur($dataHeader);
 
-        // } else {
+		for ($i=1; $i <= $baris ; $i++) { 
+		$dataDetail = array(
+			'retur_id'        => $no,
+			'product_id'       => $this->request->getPost('idproduct'.$i),
+			'product_name'        => $this->request->getPost('nameproduct'.$i),
+			'product_price' => $this->request->getPost('harga'.$i),
+			'product_qty' => $this->request->getPost('jumlah_beli'.$i)
+		);
+		$data = $model->saveDetailRetur($dataDetail);
+
+		//$data = $model->kurangiStock($dataDetail);
+		}
+		
+		if($data){
+
+			echo '200';
+
+        } else {
             
-        //     echo '300';
+            echo '300';
 
-        // }
+        }
 
     }
 }
